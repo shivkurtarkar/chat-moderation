ff017f043a8c177c6aac7c408234fa926e2afeb7
shivam <shiv.kurtarkar@gmail.com>
2022-08-15 23:14:22 +0530

    training script

--- workflow/training_workflow/training.py
+++ workflow/training_workflow/training.py
@@ -7,6 +7,13 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse
+import os
+import pickle
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
 def get_model():
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Embedding(80000, 16))
@@ -16,59 +23,14 @@ def get_model():
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
-def get_trained_tokenizer(texts):
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(
-        num_words=80000,
-        filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r',
-        lower=True,
-        split=' ',
-        char_level=False,
-        oov_token='<UNK>'
-    )
-    tokenizer.fit_on_texts(texts)
-    return tokenizer
-
-class Transformer:
-    def __init__(self, next_transformer=None):
-        self._next_transformer = next_transformer
-    def transform(self, data):
-        features = self.do_transform(data)
-        if self._next_transformer:
-            features = self._next_transformer.transform(features)
-        return features
-    def do_transform(self, data):
-        pass
-
-class TokenizerTransformer(Transformer):
-    def __init__(self, tokenizer, next_transformer=None):
-        super(TokenizerTransformer, self).__init__(next_transformer)
-        self.tokenizer = tokenizer
-    def do_transform(self, data):
-        features = self.tokenizer.texts_to_sequences(data)        
-        return features
-
-class PreprocessingTransformer(Transformer):
-    def __init__(self, next_transformer=None):
-        super(PreprocessingTransformer, self).__init__(next_transformer)
-    def do_transform(self, data):
-        embeded_data = tf.keras.preprocessing.sequence.pad_sequences(
-            data, 
-            padding='post', truncating='post', maxlen=255
-        )
-        return embeded_data
-
-
-class RedditClassifier:
-    def __init__(self, transformer, model):
-        self.transformer = transformer
-        self.model = model        
-
-    def predict(self, X):
-        features = self.transformer.transform(X)
-        predictions = self.model.predict(features)
-        return predictions    
-
+# def save_model(model, output_path):
+#     # MODEL_SAVE_DIR='../../models/tf_nsfw_text_classifer'
+#     # f'{MODEL_SAVE_DIR}/models/keras-model-w2v.h5')
+#     model.save(output_path) 
     
+# def load_model(model_path):
+#     return keras.load_model(model_path)
+  
 # def text_preprocessor(tokenizer, raw_data):
 #     # Encode training data sentenses into sequences
 #     train_sequences = tokenizer.texts_to_sequences(raw_data)
@@ -102,62 +64,61 @@ def model_evalute(model, data, labels):
         'auc': auc,
     }
 
-if __name__ == '__main__':    
-    train_dataset = 'reddit_200k_train.csv'
-    test_dataset = 'reddit_200k_test.csv'
-    DATASET_DIR='../../data/reddit'    
+def load_pickle(filename):
+    with open(filename, 'rb') as handle:
+        return pickle.load(handle)
 
-    train_dataset = RedditDataset(DATASET_DIR, train_dataset)
-    X_train = train_dataset.get_data()
-    y_train = train_dataset.get_labels()
-    print(len(X_train))
-    test_dataset = RedditDataset(DATASET_DIR, test_dataset)
-    X_test = test_dataset.get_data()
-    y_test = test_dataset.get_labels()
-    print(len(X_test))
-
-    tokenizer = get_trained_tokenizer(X_train)    
-    transformer = TokenizerTransformer(tokenizer, 
-        next_transformer=PreprocessingTransformer()
-    )
-
-    X_train_embedding = transformer.transform(X_train)
-    X_test_embedding = transformer.transform(X_test)
-    
+def run(data_path, dataset):
+    X_train, y_train = load_pickle(os.path.join(data_path, 'train.pkl'))
+    X_test, y_test = load_pickle(os.path.join(data_path, 'test.pkl'))
+        
     # Training
-    model = get_model()
-    print(model.summary())
-    history = model.fit(
-        X_train_embedding, y_train,
-        epochs=2,
-        batch_size=10000,
-        verbose=1,
-        validation_data=(X_test_embedding, y_test),
-        callbacks=[]
+    with mlflow.start_run():
+        model = get_model()
+        mlflow.log_param('dataset', dataset)
+        print(model.summary())
+        model.fit(
+            X_train, y_train,
+            epochs=20,
+            batch_size=10000,
+            verbose=1,
+            validation_data=(X_test, y_test),
+            callbacks=[]
+        )
+        
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_path',
+        default='./output'
     )
+    parser.add_argument(
+        '--dataset',
+        default='reddit'
+    )
+
+    args = parser.parse_args()
+
+    MLFLOW_TRACKING_URI = 'http://172.18.0.2:31989'
+    # MLFLOW_TRACKING_URI ='http://0.0.0.0:5000'
+    EXPERIMENT = 'text-moderation-model'
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(EXPERIMENT)
+    mlflow.tensorflow.autolog()
+    run(args.data_path, args.dataset)
     
-    # MODEL_SAVE_DIR='../../models/tf_nsfw_text_classifer'
+    # print('evaluating ... ')
+    # scores = model_evalute(model, X_test_embedding, y_test)
+    # print(scores)
 
-    # # serialize model to JSON
-    # def save_model(tokenizer, model, MODEL_SAVE_DIR):
-    #     model_json = model.to_json()
-    #     with open(f'{MODEL_SAVE_DIR}/keras-model-w2v.json', 'w') as json_file:
-    #         json_file.write(model_json)        
-    #     # serialize weight to HDF5
-    #     model.save(f'{MODEL_SAVE_DIR}/models/keras-model-w2v.h5')
-    #     print('saved model to disk')
-
-    #     # In[27]:
-    #     import pickle
-    #     with open(f'{MODEL_SAVE_DIR}/tokenizer.pickle', 'wb') as handle:
-    #         pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
-    print('evaluating ... ')
-    scores = model_evalute(model, X_test_embedding, y_test)
-    print(scores)
-
+    # save_model(model, output_path) 
     ## inference
-    reddit_classifier = RedditClassifier(transformer, model)
-    data = [['f*ck off bitch']]
-    prediction = reddit_classifier.predict(data)
-    print(prediction)
+    # model = load_model()
+    # tokenizer = load_tokenizer()
+
+    # reddit_classifier = RedditClassifier(transformer, model)
+    # data = [['f*ck off bitch']]
+    # prediction = reddit_classifier.predict(data)
+    # print(prediction)
