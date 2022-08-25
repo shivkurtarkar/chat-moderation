543412deeaa3fc991b017f601977a56a63126dd0
shivam <shiv.kurtarkar@gmail.com>
2022-08-25 11:24:44 +0530

    renamed download file

--- workflow/download.py
+++
@@ -1,6 +0,0 @@
-# export KAGGLE_USERNAME=datadinosaur
-# export KAGGLE_KEY=xxxxxxxxxxxxxx
-import  kaggle
-kaggle.api.authenticate()
-dataset = 'areeves87/rscience-popular-comment-removal'
-kaggle.api.dataset_download_files(dataset, path = './output', unzip=True)
