1f23e4ebc7e8a11bb0140e1d836fff7bd41a36a1
shivam <shiv.kurtarkar@gmail.com>
2022-08-28 06:38:12 +0530

    fix dataset

--- workflow/reddit_dataset.py
+++ workflow/reddit_dataset.py
@@ -5,9 +5,13 @@ class DataSource:
         pass
 
 class RedditDataset(DataSource):
-    def __init__(self, filename):                
-        self.columns = ["prev_idx", "body", "score", "parent_id", "id", "created_date", "retrieved_date", "removed"]
-        # self.columns = ["","X","BODY","REMOVED"]
+    def __init__(self, filename):
+        if 'reddit_200k' in filename:
+            self.columns = ["prev_idx", "body", "score", "parent_id", "id", "created_date", "retrieved_date", "removed"]
+        else:
+            self.columns = ["","X","body","removed"]            
+        self.body_key = 'body'
+        self.removed_key = 'removed'
         self.filename = filename
     def _get_dataframe(self, filename):
         dataset = pd.read_csv(
@@ -18,15 +22,11 @@ class RedditDataset(DataSource):
         )
         return dataset
     def get_data(self):
-        dataset = self._get_dataframe(self.filename)        
-        body_key = 'body'
-        # body_key = 'BODY'
-        return dataset[body_key]   
+        dataset = self._get_dataframe(self.filename)
+        return dataset[self.body_key]   
     def get_labels(self):
         dataset = self._get_dataframe(self.filename)
-        removed_key = 'removed'
-        # removed_key = 'REMOVED'
-        return dataset[removed_key]
+        return dataset[self.removed_key]
 
 if __name__ == '__main__':
     train_dataset = 'reddit_200k_train.csv'
